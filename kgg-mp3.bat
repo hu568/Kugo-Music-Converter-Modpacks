aa0ef991dfee6cd5df0595f82b3a01e2b15290b1
hu568 <3381316366@qq.com>
2025-05-03 13:10:21 +0800

    Update kgg-mp3.bat

--- kgg-mp3.bat
+++ kgg-mp3.bat
@@ -15,8 +15,8 @@ for %%f in (*.kgg) do (
     :: 重命名生成的 .ogg 文件
     ren "!filename!_kgg-dec.ogg" "!filename!.ogg"
     
-    :: 使用 ffmpeg 将 .ogg 转换为 .mp3（最高品质，保留元数据）
-    ffmpeg.exe -i "!filename!.ogg" -q:a 0 -map_metadata 0 "kgm-vpr-out\!filename!.mp3"
+    :: 使用 kgm-vpr-out 文件夹中的 ffmpeg 将 .ogg 转换为 .mp3（最高品质，保留元数据）
+    "kgm-vpr-out\ffmpeg.exe" -i "!filename!.ogg" -q:a 0 -map_metadata 0 "kgm-vpr-out\!filename!.mp3"
     
     :: 删除中间生成的 .ogg 文件
     del "!filename!.ogg"
